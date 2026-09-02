cc4a9130db3c7c3c41a4522293a7e06488a18ca0
HaYTo <korazhayto@gmail.com>
2026-09-02 15:34:38 +0300

    Update Firebase config to new GitHub specific project

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -3,12 +3,13 @@ import { getAuth, GithubAuthProvider, signInWithPopup, signInWithCredential, sig
 import { getFirestore, doc, getDoc, setDoc, onSnapshot } from '../lib/firebase/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmAQjLcdlpwDWaAM7TAmORs0qWHFw8dcE",
-  authDomain: "haytool-startpage-v4.firebaseapp.com",
-  projectId: "haytool-startpage-v4",
-  storageBucket: "haytool-startpage-v4.firebasestorage.app",
-  messagingSenderId: "2909602953",
-  appId: "1:2909602953:web:d6accc61f831c5af543258"
+  apiKey: "AIzaSyAC68Qs2ct4TW8i1TS_G2gNVaw9ur4Cyr0",
+  authDomain: "haytool-cloud-startpage-github.firebaseapp.com",
+  projectId: "haytool-cloud-startpage-github",
+  storageBucket: "haytool-cloud-startpage-github.firebasestorage.app",
+  messagingSenderId: "524900247283",
+  appId: "1:524900247283:web:7905a99380660ec3adf986",
+  measurementId: "G-L4B22FC4Z3"
 };
 
 // Initialize Firebase
@@ -17,4 +18,3 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db, GithubAuthProvider, signInWithPopup, signInWithCredential, signOut, onAuthStateChanged, doc, getDoc, setDoc, onSnapshot };
-
